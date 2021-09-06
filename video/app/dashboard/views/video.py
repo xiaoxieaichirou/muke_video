dc3c48300b1f537391f4b8444c14bc73387fcca9
xiechunwei <13226512215@163.com>
2021-09-06 18:02:03 +0800

    创建外链视频

--- video/app/dashboard/views/video.py
+++ video/app/dashboard/views/video.py
@@ -4,6 +4,8 @@ from django.views.generic import View
 from django.shortcuts import redirect, reverse
 from app.libs.base_render import render_to_response
 from app.utils.permission import dashboard_auth
+from app.model.video import VideoType, FromType, NationalityType, Video
+from app.utils.common import check_and_get_video_type
 
 
 class ExternalVideo(View):
@@ -12,7 +14,14 @@ class ExternalVideo(View):
     @dashboard_auth
     def get(self, request):
 
-        return render_to_response(request, self.TEMPLATE)
+        error = request.GET.get('error', '')
+        data = {'error': error}
+
+        # 排除FromType为custom的视频
+        videos = Video.objects.exclude(from_to=FromType.custom.value)
+        data['videos'] = videos
+
+        return render_to_response(request, self.TEMPLATE, data=data)
 
     def post(self, request):
         name = request.POST.get('name')
@@ -20,5 +29,24 @@ class ExternalVideo(View):
         video_type = request.POST.get('video_type')
         nationality = request.POST.get('nationality')
         from_to = request.POST.get('from_to')
+        info = request.POST.get('info')
+
+        if not all([name, image, video_type, nationality, from_to, info]):
+            return redirect('{}?error={}'.format(reverse('external_video'), '缺少必要字段'))
+
+        result = check_and_get_video_type(VideoType, video_type, '非法的视频类型')
+        if result.get('code') != 0:
+            return redirect('{}?error={}'.format(reverse('external_video'), result['msg']))
+
+        result = check_and_get_video_type(NationalityType, nationality, '非法的国籍')
+        if result.get('code') != 0:
+            return redirect('{}?error={}'.format(reverse('external_video'), result['msg']))
+
+        result = check_and_get_video_type(FromType, from_to, '非法的来源')
+        if result.get('code') != 0:
+            return redirect('{}?error={}'.format(reverse('external_video'), result['msg']))
+
+        Video.objects.create(name=name, image=image, video_type=video_type, nationality=nationality,
+                             from_to=from_to, info=info)
 
         return redirect(reverse('external_video'))
